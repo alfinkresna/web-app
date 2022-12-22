2b2b26cd66cb58fb2a80dd42fe159661deb23075
A l f i n d o <75003444+alfinkresna@users.noreply.github.com>
2022-12-23 06:26:12 +0700

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -31,16 +31,11 @@ def about():
     ''', unsafe_allow_html=True)
 
     st.markdown(
+    <center>
     '<a href="https://www.instagram.com/alfin.kresna_">'
     '<img src="https://www.instagram.com/static/images/ico/favicon-192.png/68d99ba29cc8.png" '
-    'style="width: 32px; height: 32px; margin: 10px;"></a>',
+    'style="width: 32px; height: 32px; margin: 10px;">My Instagram</a></center>',
     unsafe_allow_html=True,)
-
-    st.markdown(
-    '<a href="https://www.instagram.com/alfin.kresna_" style="font-size: 16px;">'
-    'Follow us on Instagram</a>',
-    unsafe_allow_html=True,)
-
 
 def main():
     about()
